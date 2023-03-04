3ac5021e6beeccdbd78f7caeb05867abcccecb03
YDH <swimmingpolar@gmail.com>
2023-03-04 18:24:11 +0900

    Add OpenAI configuration and test API call

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-export * from './lib/async';
-export * from './lib/number';
+import { configSecrets } from './lib/env';
+configSecrets();
+import { Configuration, OpenAIApi } from 'openai';
+
+// Set up the OpenAI API
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function init() {
+  // Test the API
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: 'Say this is a test',
+    temperature: 0,
+    max_tokens: 7,
+  });
+
+  console.log(response.data);
+}
+
+init();
